e61229929d77f5dc4e2034f3b66a325f425ffa04
Andrew Storrow <andrew.storrow@gmail.com>
2023-05-26 13:57:11 -0500

    added intitial files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace ComputerParts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true";
+
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+
+            string sqlCommand = "SELECT GETDATE()";
+
+            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+            Console.WriteLine(rightNow);
+            Console.WriteLine("Computer Parts!");
+        }
+    }
+}
